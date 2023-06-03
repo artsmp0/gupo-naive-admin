9e2e4a912caf03454706522b8665993f1213055a
沈孟平 <shen990206@gmail.com>
2023-06-03 23:21:13 +0800

    feat: use view transition

--- src/layout/components/Header.vue
+++ src/layout/components/Header.vue
@@ -7,7 +7,7 @@ import {
   SettingsOutline
 } from '@vicons/ionicons5';
 import { CompressOutlined, ExpandOutlined, ReloadOutlined } from '@vicons/antd';
-import { isDark, toggleDark } from '@/composables';
+import { isDark } from '@/composables';
 import { useFullscreen } from '@vueuse/core';
 import { NAvatar, NIcon, NText } from 'naive-ui';
 import { useUserStore } from '@/stores/user';
@@ -102,6 +102,39 @@ const onSelect = (key: string) => {
 };
 
 const appName = computed(() => import.meta.env.VITE_APP_TITLE);
+
+function toggleDark(event: MouseEvent) {
+  const isAppearanceTransition =
+    // @ts-ignore
+    document.startViewTransition && !window.matchMedia('(prefers-reduced-motion: reduce)').matches;
+
+  if (!isAppearanceTransition) {
+    isDark.value = !isDark.value;
+    return;
+  }
+
+  const x = event.clientX;
+  const y = event.clientY;
+  const endRadius = Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y));
+  // @ts-expect-error: Transition API
+  const transition = document.startViewTransition(async () => {
+    isDark.value = !isDark.value;
+    await nextTick();
+  });
+  transition.ready.then(() => {
+    const clipPath = [`circle(0px at ${x}px ${y}px)`, `circle(${endRadius}px at ${x}px ${y}px)`];
+    document.documentElement.animate(
+      {
+        clipPath: isDark.value ? [...clipPath].reverse() : clipPath
+      },
+      {
+        duration: 500,
+        easing: 'ease-out',
+        pseudoElement: isDark.value ? '::view-transition-old(root)' : '::view-transition-new(root)'
+      }
+    );
+  });
+}
 </script>
 
 <template>
@@ -112,7 +145,7 @@ const appName = computed(() => import.meta.env.VITE_APP_TITLE);
       <Breadcrumbs />
     </div>
     <div class="shrink-0" flex="~ items-center gap-16">
-      <NButton   secondary circle  @click="reload">
+      <NButton secondary circle @click="reload">
         <template #icon>
           <NIcon><ReloadOutlined /></NIcon>
         </template>
@@ -125,7 +158,7 @@ const appName = computed(() => import.meta.env.VITE_APP_TITLE);
           </NIcon>
         </template>
       </NButton>
-      <NButton circle secondary @click="toggleDark()">
+      <NButton circle secondary @click="toggleDark">
         <template #icon>
           <NIcon>
             <MoonOutline v-if="isDark" />
